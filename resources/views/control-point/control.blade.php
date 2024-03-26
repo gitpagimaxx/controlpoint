0308582a9632c3162ba1368d95d4b7bc406acb3d
Diego Oliveira <diego@pagimaxx.com>
2024-03-25 21:40:52 -0300

    feat: tela controlpoint alinhamento form e tabela

--- resources/views/control-point/control.blade.php
+++ resources/views/control-point/control.blade.php
@@ -12,61 +12,76 @@
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    
                     <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <span class="mx-auto"></span>
-                        <div class="mb-5 mx-auto pb-5">
-                            <h1 class="text-2xl">{{ \Carbon\Carbon::parse($today)->format('d/m/Y') }}</h1>
-                        </div>
-                        <div class="mt-4 mb-2">
-                            <x-input-label for="ClockIn" :value="__('Entrada')" />
-                            <x-text-input id="ClockIn" name="ClockIn" type="time" class="mt-1 md:w-32 lg:w-48" :value="old('ClockIn', $controlPointToday->ClockIn ?? '00:00')" autofocus />
-                            <x-input-error class="mt-2" :messages="$errors->get('ClockIn')" />
-                        </div>
-                        <div class="mt-4">
-                            <x-input-label for="LunchTimeIn" :value="__('Saída Almoço')" />
-                            <x-text-input id="LunchTimeIn" name="LunchTimeIn" type="time" :value="old('LunchTimeIn', $controlPointToday->LunchTimeIn ?? '00:00')" class="mt-1" />
-                            <x-input-error class="mt-2" :messages="$errors->get('LunchTimeIn')" />
-                        </div>
-                        <div class="mt-4">
-                            <x-input-label for="LunchTimeOut" :value="__('Retorno Almoço')" />
-                            <x-text-input id="LunchTimeOut" name="LunchTimeOut" type="time" :value="old('LunchTimeOut', $controlPointToday->LunchTimeOut ?? '00:00')" class="mt-1" />
-                            <x-input-error class="mt-2" :messages="$errors->get('LunchTimeOut')" />
-                        </div>
-                        <div class="mt-4">
-                            <x-input-label for="ClockOut" :value="__('Fim do dia')" />
-                            <x-text-input id="ClockOut" name="ClockOut" type="time" :value="old('ClockOut', $controlPointToday->ClockOut ?? '00:00')" class="mt-1" />
-                            <x-input-error class="mt-2" :messages="$errors->get('ClockOut')" />
-                        </div>
-                    </div>
-                    <div class="p-6 text-gray-900 dark:text-gray-100">
-                        <div class="mb-2">
-                            <x-primary-button aria-setsize="lg">{{ __('Salvar') }}</x-primary-button>
-                        </div>
-                    </div>
 
-                    <div>
-                        <table class="table-auto text-white">
-                            <thead>
-                                <tr>
-                                    <th>Data</th>
-                                    <th>Entrada</th>
-                                    <th>Saída Almoço</th>
-                                    <th>Retorno Almoço</th>
-                                    <th>Saída</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach($controlPoints as $controlPoint)
-                                <tr>
-                                    <td>{{ \Carbon\Carbon::parse($controlPoint->Date)->format('d/m/Y') }}</td>
-                                    <td>{{ \Carbon\Carbon::parse($controlPoint->ClockIn)->format('H:i') }}</td>
-                                    <td>{{ \Carbon\Carbon::parse($controlPoint->LunchTimeIn)->format('H:i') }}</td>
-                                    <td>{{ \Carbon\Carbon::parse($controlPoint->LunchTimeOut)->format('H:i') }}</td>
-                                    <td>{{ \Carbon\Carbon::parse($controlPoint->ClockOut)->format('H:i') }}</td>
-                                </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
+                        <div class="flex justify-between">
+                            <div>
+                                <div class="flex-1">
+                                    <x-input-label for="ClockIn" :value="__('Entrada')" />
+                                    <x-text-input id="ClockIn" name="ClockIn" type="time" class="mt-1" :value="old('ClockIn', $controlPointToday->ClockIn ?? '00:00')" autofocus />
+                                    <x-input-error class="mt-2" :messages="$errors->get('ClockIn')" />
+                                </div>
+                            </div>
+                            <div>
+                                <div class="flex-1">
+                                    <x-input-label for="LunchTimeIn" :value="__('Saída Almoço')" />
+                                    <x-text-input id="LunchTimeIn" name="LunchTimeIn" type="time" :value="old('LunchTimeIn', $controlPointToday->LunchTimeIn ?? '00:00')" class="mt-1" />
+                                    <x-input-error class="mt-2" :messages="$errors->get('LunchTimeIn')" />
+                                </div>
+                            </div>
+                            <div>
+                                <div class="flex-1">
+                                    <x-input-label for="LunchTimeOut" :value="__('Retorno Almoço')" />
+                                    <x-text-input id="LunchTimeOut" name="LunchTimeOut" type="time" :value="old('LunchTimeOut', $controlPointToday->LunchTimeOut ?? '00:00')" class="mt-1" />
+                                    <x-input-error class="mt-2" :messages="$errors->get('LunchTimeOut')" />
+                                </div>
+                            </div>
+                            <div>
+                                <div class="flex-1">
+                                    <x-input-label for="ClockOut" :value="__('Fim do dia')" />
+                                    <x-text-input id="ClockOut" name="ClockOut" type="time" :value="old('ClockOut', $controlPointToday->ClockOut ?? '00:00')" class="mt-1" />
+                                    <x-input-error class="mt-2" :messages="$errors->get('ClockOut')" />
+                                </div>
+                            </div>
+                            <div>
+                                <div class="flex self-end m-5">
+                                    &nbsp;<br>
+                                    <x-primary-button aria-setsize="lg">{{ __('Salvar') }}</x-primary-button>
+                                </div>
+                            </div>
+                          </div>
+
+                        <div class="divide-y divide-white">&nbsp;</div>
+                    
+                        <div class="flex justify-between mt-7">
+                            <div class="flex-1">
+                                <table class="table-auto border">
+                                    <thead>
+                                        <tr class="w-full">
+                                            <th class="font-bold p-2 border-b text-left">Data</th>
+                                            <th class="font-bold p-2 border-b text-center">Entrada</th>
+                                            <th class="font-bold p-2 border-b text-center">Saída Almoço</th>
+                                            <th class="font-bold p-2 border-b text-center">Retorno Almoço</th>
+                                            <th class="font-bold p-2 border-b text-left">Saída</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach($controlPoints as $controlPoint)
+                                        <tr>
+                                            <td class="p-2 border-b text-left">{{ \Carbon\Carbon::parse($controlPoint->Date)->format('d/m/Y') }}</td>
+                                            <td class="p-2 border-b text-center">{{ \Carbon\Carbon::parse($controlPoint->ClockIn)->format('H:i') }}</td>
+                                            <td class="p-2 border-b text-center">{{ \Carbon\Carbon::parse($controlPoint->LunchTimeIn)->format('H:i') }}</td>
+                                            <td class="p-2 border-b text-center">{{ \Carbon\Carbon::parse($controlPoint->LunchTimeOut)->format('H:i') }}</td>
+                                            <td class="p-2 border-b text-left">{{ \Carbon\Carbon::parse($controlPoint->ClockOut)->format('H:i') }}</td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                            
+                        </div>
+
                     </div>
                 </div>
             </div>
